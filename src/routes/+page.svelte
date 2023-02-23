f8fd2924ebab9252d2ffb5b49be565e756ad7b7f
BraianPita <braianx100@gmail.com>
2023-02-22 23:03:59 -0600

    Todo items table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang='ts'>
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import { Alert, Card, Button } from 'flowbite-svelte';
-	import db from '../lib/db-config';
+	import { Alert } from 'flowbite-svelte';
 
-	import { todos, loadTodos } from '../stores/todoStore';
-	import type { TodoItem } from '../stores/todoStore';
-
-	let todo_data: TodoItem[] = [];
+	import { loadTodos } from '../stores/todoStore';
+	import TodoItem from './TodoItem.svelte';
 
 	// async function getAllTodo() {
 	// 	let { data: Todo, error } = await db
@@ -29,47 +26,9 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		This is edited!
-	</h2>
-
-	<div class="p-8">
-		<Alert>
-			<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-		</Alert>
-	</div>
 
 	<button on:click={loadTodos}>Test</button>
-	<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-		{#each $todos as todo (todo.uid)}
-		<Card class="mb-4">
-		<div class="flex items-center justify-between">
-		<h4 class="text-lg font-semibold">{todo.description}</h4>
-		<span class="text-sm text-gray-500">Due date:{todo.due_date}</span>
-		</div>
-		<div class="flex items-center justify-end">
-		<Button variant="primary" size="sm" on:click={() => console.log(todo.uid)}>
-		{#if todo.done}
-		<i class="fas fa-check"></i> Done
-		{:else}
-		<i class="fas fa-times"></i> Not done
-		{/if}
-		</Button>
-		</div>
-		</Card>
-		{/each}
-	  </div>
+	<TodoItem></TodoItem>
 
 </section>
 
---
+++ src/routes/TodoItem.svelte
@@ -0,0 +1,39 @@
+<script>
+// @ts-nocheck
+
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox} from 'flowbite-svelte';
+    import { todos } from '../stores/todoStore';
+</script>
+
+<Table hoverable={true}>
+  <TableHead>
+    <TableHeadCell class="!p-4">
+      <Checkbox />
+    </TableHeadCell>
+    <TableHeadCell>Description</TableHeadCell>
+    <TableHeadCell>Due</TableHeadCell>
+    <TableHeadCell>Status</TableHeadCell>
+    <TableHeadCell>
+      <span class="sr-only"> Edit </span>
+    </TableHeadCell>
+  </TableHead>
+  <TableBody class="divide-y" >
+    
+    {#each $todos as todo (todo.uid)}
+    <TableBodyRow>
+        <TableBodyCell class="!p-4">
+          <Checkbox />
+        </TableBodyCell>
+        <TableBodyCell>{todo.description}</TableBodyCell>
+        <TableBodyCell>{todo.due_date}</TableBodyCell>
+        <TableBodyCell>{todo.done}</TableBodyCell>
+        <TableBodyCell>
+          <a href="/tables" class="font-medium text-blue-600 hover:underline dark:text-blue-500">
+            Edit
+          </a>
+        </TableBodyCell>
+    </TableBodyRow>
+    {/each}
+    
+  </TableBody>
+</Table>
